84da28d14e90e148368b7817c55261823c8cfd05
abhishekn123 <abhiabhishekn164@gmail.com>
2020-06-26 23:04:42 +0530

    searchbarAdded

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule, 
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule
   ],
-  exports:[  MatButtonModule,MatToolbarModule]
+  exports:[  MatButtonModule,MatToolbarModule,MatFormFieldModule]
 })
 export class AngularMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { AppComponent } from './app.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchBarComponent
   ],
   imports: [
-  BrowserModule,
-  BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AngularMaterialModule,
-  
   ],
   providers: [],
   bootstrap: [AppComponent]
